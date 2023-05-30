a91b4dccec1c1681ce888abeedb21b3c7e4c2379
dcruz-nc <dcruz.nc@gmail.com>
2023-05-29 21:29:04 -0400

    Update FXMain.java
    Testing edits

--- DataSync/src/databasegui/FXMain.java
+++ DataSync/src/databasegui/FXMain.java
@@ -14,7 +14,7 @@ public class FXMain extends Application {
     public void start(Stage stage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("start-menuFXML.fxml"));
-            stage.setTitle("DataSync");                   
+            stage.setTitle("DataSync By David Cruz");                   
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
